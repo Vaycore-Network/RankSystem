682f4f8ef6e32157704cdf475e43c8a4f11170df
c4vxl <manage@c4vxl.de>
2026-03-08 12:03:12 +0100

    Implemented RankManager

---
+++ src/main/kotlin/de/c4vxl/papertemplate/data/Rank.kt
@@ -0,0 +1,32 @@
+package de.c4vxl.papertemplate.data
+
+import org.bukkit.Bukkit
+import org.bukkit.OfflinePlayer
+import java.util.*
+
+/**
+ * Data class representing a rank
+ * @param name The name of the rank
+ * @param position The position in tab list
+ * @param prefix The prefix prepended to the name of players with this rank
+ * @param suffix The suffix appended to the name of players with this rank
+ * @param playerIDs The ids of the players that have this rank
+ * @param permissions The permissions this rank grants
+ */
+data class Rank(
+    val name: String,
+    var position: String = "000",
+    var prefix: String = "",
+    var suffix: String = "",
+    var playerIDs: MutableList<UUID> = mutableListOf(),
+    var permissions: MutableList<String> = mutableListOf()
+) {
+    val players: List<OfflinePlayer>
+        get() = playerIDs.map { Bukkit.getOfflinePlayer(it) }
+
+    /**
+     * Saves changes to database
+     */
+    fun save() =
+        Database.update(this)
+}
