0d57fd85266c280236fb86dfa4202acb9383d020
c4vxl <manage@c4vxl.de>
2026-03-09 20:12:22 +0100

    Added handler for rank unregistered

--- src/main/kotlin/de/c4vxl/ranksystem/plugin/handlers/DisplayHandler.kt
+++ src/main/kotlin/de/c4vxl/ranksystem/plugin/handlers/DisplayHandler.kt
@@ -3,6 +3,7 @@ package de.c4vxl.ranksystem.plugin.handlers
 import de.c4vxl.ranksystem.Main
 import de.c4vxl.ranksystem.data.Database
 import de.c4vxl.ranksystem.data.Rank
+import de.c4vxl.ranksystem.event.data.RankUnregisterEvent
 import de.c4vxl.ranksystem.event.update.*
 import de.c4vxl.ranksystem.ranks.RankManager
 import net.kyori.adventure.text.Component
@@ -24,13 +25,14 @@ class DisplayHandler : Listener {
         Bukkit.getPluginManager().registerEvents(this, Main.instance)
     }
 
-    /**
-     * Returns the team used for displaying a rank to a player
-     * @param scoreboard The scoreboard
-     * @param rank The rank
-     */
-    private fun getSBTeam(scoreboard: Scoreboard, rank: Rank) =
-        "rank_${rank.position}_${rank.name}".let {
+    private fun getSBTeam(position: String, rank: String, scoreboard: Scoreboard = Bukkit.getScoreboardManager().mainScoreboard) =
+        /**
+         * Returns the team used for displaying a rank to a player
+         * @param position The position of the rank
+         * @param rank The name of the rank
+         * @param scoreboard The scoreboard
+         */
+        "rank_${position}_${rank}".let {
             scoreboard.getTeam(it)
                 ?: scoreboard.registerNewTeam(it)
         }
@@ -41,7 +43,7 @@ class DisplayHandler : Listener {
      * @param player The player to handle the displaying for
      */
     private fun handle(rank: Rank, player: Player) {
-        val team = getSBTeam(Bukkit.getScoreboardManager().mainScoreboard, rank)
+        val team = getSBTeam(rank.position, rank.name)
 
         // Remove player from all other rank teams
         player.scoreboard.teams
@@ -111,7 +113,18 @@ class DisplayHandler : Listener {
 
     @EventHandler
     fun onPositionChange(event: RankPositionUpdateEvent) {
+        // Delete old team
+        getSBTeam(event.oldPosition, event.rank.name)
+            .unregister()
+
         event.rank.players.forEach { displayHighest(it) }
+    }
+
+    @EventHandler
+    fun onUnregister(event: RankUnregisterEvent) {
+        // Delete team
+        getSBTeam(event.rank.position, event.rank.name)
+            .unregister()
     }
 
     @EventHandler
